db8b38d267b51462d6ea59e0af36fb9c01173ca3
DollyChen_CYC <dolly726033@gmail.com>
2022-02-10 21:08:47 +0800

    feat: [backend] add user login controller

--- server/controllers/api/user-controller.js
+++ server/controllers/api/user-controller.js
@@ -1,13 +1,44 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { User } = require('../../models')
 
 const userController = {
   getUsers: (req, res) => {
     res.send('user list')
   },
+  login: (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password) return res.send({ status: 'error', message: 'All fields are required!' })
+
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) return res.status(401).json({ status: 'error', message: 'User not found.' })
+        const isPasswordPassed = bcrypt.compareSync(password, user.password)
+        if (!isPasswordPassed) return res.status(401).json({ status: 'error', message: 'The password is incorrect.' })
+
+        // sign Token
+        const { id, name, email, cell_phone: cellPhone, is_admin: isAdmin } = user
+        const payload = { id }
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' })
+
+        return res.json({
+          status: 'success',
+          message: 'OK',
+          token,
+          user: {
+            id,
+            name,
+            email,
+            cellPhone,
+            isAdmin
+          },
+          isAuthenticated: true
+        })
+      })
+  },
   signUp: (req, res) => {
     const { email, name, password, confirmPassword, cellPhone } = req.body
-    
+
     if (!(email && name && password && confirmPassword && cellPhone)) {
       return res.json({ status: 'error', message: 'Some required fields are empty!' })
     }
@@ -30,7 +61,7 @@ const userController = {
       })
       .then(user => {
         console.log('--Registration success!--')
-        return res.json({ status: 'success', message: 'Registration success!'})
+        return res.json({ status: 'success', message: 'Registration success!' })
       })
       .catch(err => console.log(err))
   }
