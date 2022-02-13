ee6761361aaa700b3820e6cab2e255ceafbb8301
DollyChen_CYC <dolly726033@gmail.com>
2022-02-13 19:09:13 +0800

    feat: add config for heroku

--- server/app.js
+++ server/app.js
@@ -8,11 +8,17 @@ const methodOverride = require('method-override')
 const app = express()
 const routes = require('./routes')
 
-const PORT = process.env.SERVER_PORT
+const PORT = process.env.PORT
 
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(PORT, () => console.log(`App server is running on http://localhost:${PORT}`))
+app.listen(PORT, () => {
+  if (process.env.NODE_ENV == 'production') {
+    console.log(`App server is running.`)
+  } else {
+    console.log(`App server is running on http://localhost:${PORT}`)
+  }
+})
